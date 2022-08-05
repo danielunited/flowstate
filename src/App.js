0bdf37065daff8a535fecc1275175c0ac14dc63c
Daniel Alter <daniel.united@windowslive.com>
2022-08-05 23:24:34 +0300

    Added timer, upgraded editor

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import LocalEditor from "./lib/components/LocalEditor";
 import FlowButton from "./lib/components/FlowButton";
 import SaveButton from "./lib/components/SaveButton";
 import FocusPopup from "./lib/components/FocusPopup";
+import Timer from "./lib/components/Timer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div className="app-button-container">
         <FlowButton />
         <SaveButton />
+        {/* <Timer /> */}
       </div>
       {/* <FocusPopup /> */}
     </div>
