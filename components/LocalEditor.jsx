51cfe9ea01ba4e828816dc1d7087bbe03954df12
NivEz <niv.cyber@gmail.com>
2022-09-09 18:47:29 +0300

    rendering default value from localStorage

--- components/LocalEditor.jsx
+++ components/LocalEditor.jsx
@@ -6,6 +6,7 @@ import {debounce} from "lodash";
 import RichMarkdownEditor from "flowstate-editor";
 import theme from "../constants/theme";
 import {useDidMount} from "../hooks/useDidMount";
+import {TwitterTweetEmbed} from "react-twitter-embed";
 
 // import BridgeManager from "../src/lib/BridgeManager";
 
@@ -19,9 +20,44 @@ class YoutubeEmbed extends React.Component {
     }
 }
 
+class TwitterEmbed extends React.Component {
+    render() {
+        const {attrs} = this.props;
+        const statusId = attrs.matches[1];
+
+        return <TwitterTweetEmbed tweetId={statusId} placeholder={"Loading tweet..."}/>;
+    }
+}
+
+class ImageEmbed extends React.Component {
+    render() {
+        const {attrs} = this.props;
+        const imageUrl = attrs.matches[1];
+
+        return {imageUrl};
+    }
+}
+
 
 export const LocalEditor = () => {
+    const [defaultValue, setDefaultValue] = useState("");
+    console.log("-> defaultValue", defaultValue);
     const didMount = useDidMount()
+
+    useEffect(() => {
+        setDefaultValue(localStorage.getItem("saved"))
+        // const text = localStorage.getItem("saved")
+        // setDefaultValue(setMarkdown(text))
+    }, [])
+
+
+    const setMarkdown = (text) => {
+        let markdown = text.replace(/(\n{2})(\n+)(?!:::)(?!---)/g, (m, p, q) => p + q.replace(/(\n)/g, "\\$1"));
+        if (markdown === "") {
+            markdown = "\n";
+        }
+        return markdown;
+    }
 
     const onChange = debounce(value => {
         // access localStorage only after the component mounted
@@ -45,6 +81,47 @@ export const LocalEditor = () => {
                     onChange={onChange}
                     theme={theme}
                     className="gKsMQS"
+                    value={defaultValue}
+                    onSearchLink={(searchTerm) => {
+                        const results = this.editor.getHeadings();
+
+                        return results
+                            .map((result) => {
+                                return {title: result.title, subtitle: `H${result.level}`, url: "#" + result.id};
+                            })
+                            .filter((result) => result.title.match(searchTerm));
+                    }}
+                    onClickLink={(href, event) => {
+                        let url = new URL(href);
+                        if (url.pathname == "/" && url.hash.length > 0) {
+                            this.editor.scrollToAnchor(url.hash);
+                        } else {
+                            window.open(href, "_blank");
+                        }
+                    }}
+                    embeds={[
+                        {
+                            title: "Twitter",
+                            matcher: (url) => {
+                                return url.match(/^https?:\/\/twitter.com\/[\d\w_-]+\/status\/(\d+)/);
+                            },
+                            component: TwitterEmbed,
+                        },
+                        {
+                            title: "YouTube",
+                            matcher: (url) => {
+                                return url.match(/(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([a-zA-Z0-9_-]{11})$/i);
+                            },
+                            component: YoutubeEmbed,
+                        },
+                        {
+                            title: "Image",
+                            matcher: (url) => {
+                                return url.match("^https?://.*\\.(?:png|jpg|jpeg|gif)$");
+                            },
+                            component: ImageEmbed,
+                        },
+                    ]}
                 />
             </div>
         </div>
