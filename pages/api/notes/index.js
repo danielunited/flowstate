f4d6ba84fdef860f4590c60a902aa6e80e93533e
NivEz <niv.cyber@gmail.com>
2022-09-16 22:36:22 +0300

    added cookie logic with token and expiry of 1 year

--- pages/api/notes/index.js
+++ pages/api/notes/index.js
@@ -1,13 +1,18 @@
-import { createNote } from '../../../lib/api/notes/service';
+import { createNote, handleCookieWithToken } from '../../../lib/api/services/notes.service';
 
 export default function handler(req, res) {
 	if (req.method === 'POST') {
 		const { content } = req.body;
 		// validate
 
-		const newNote = createNote(content);
-
-		res.json({ note: newNote });
+		const newNoteData = createNote(content);
+		const finalCookie = handleCookieWithToken(
+			req.cookies.notesTokens,
+			newNoteData.id,
+			newNoteData.noteToken,
+		);
+		res.setHeader('Set-Cookie', finalCookie);
+		res.json({ id: newNoteData.id });
 	} else {
 		console.log('an error has occurred');
 	}
