985e385eaed55abffcb805d41a67179448f5c1db
NivEz <niv.cyber@gmail.com>
2022-09-17 19:58:44 +0300

    added mostRecent (is editing) status logic

--- pages/api/notes/[noteId].js
+++ pages/api/notes/[noteId].js
@@ -1,19 +1,23 @@
 import {
-	getUserTokenFromCookie,
-	updateNoteContentById,
-} from '../../../lib/api/services/notes.service';
+	getUserTokenFromCookie, setNoteInEditingStatus,
+	updateNoteContentById
+} from "../../../lib/api/services/notes.service";
 
 export default function handler(req, res) {
 	if (req.method === 'PATCH') {
-		const { noteId } = req.query;
+		const { noteId, setInEditMode } = req.query;
 		const { content } = req.body;
 		const { notesTokens } = req.cookies;
 		const token = getUserTokenFromCookie(noteId, notesTokens);
 		if (!token) {
 			return res.status(401).end();
 		}
-		const updated = updateNoteContentById(noteId, content, token);
-		res.json({ note: updated });
+		if (setInEditMode) {
+			setNoteInEditingStatus(noteId, token)
+		} else {
+			const updated = updateNoteContentById(noteId, content, token);
+			res.json({ note: updated });
+		}
 	} else {
 		console.log('an error has occurred');
 	}
