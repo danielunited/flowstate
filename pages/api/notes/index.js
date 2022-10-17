1beb1a4ae69adba217037c6fd79513648a87e5ed
NivEz <niv.cyber@gmail.com>
2022-10-18 01:39:29 +0300

    added Note model and in progress of implementing the new model in the notes DAL and service

--- pages/api/notes/index.js
+++ pages/api/notes/index.js
@@ -1,19 +1,23 @@
 import { createNote, handleCookieWithToken } from '../../../lib/api/services/notes.service';
+import { apiMiddleware } from '../../../lib/api/middleware';
 
-export default function handler(req, res) {
+const postHandler = async (req) => {
 	if (req.method === 'POST') {
 		const { content } = req.body;
 		// validate
 
-		const newNoteData = createNote(content);
-		const finalCookie = handleCookieWithToken(
-			req.cookies.notesTokens,
-			newNoteData.id,
-			newNoteData.noteToken,
-		);
-		res.setHeader('Set-Cookie', finalCookie);
-		res.json({ id: newNoteData.id });
+		const newNoteData = await createNote(content);
+		// const finalCookie = handleCookieWithToken(
+		// 	req.cookies.notesTokens,
+		// 	newNoteData.id,
+		// 	newNoteData.noteToken,
+		// );
+		// res.setHeader('Set-Cookie', finalCookie);
+		// res.json({ id: newNoteData.id });
+		return newNoteData
 	} else {
 		console.log('an error has occurred');
 	}
-}
+};
+
+export default apiMiddleware({ post: postHandler });
