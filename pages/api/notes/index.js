7f0b8d9564e9dee69d804f7fccc258d39eb21d9a
NivEz <niv.cyber@gmail.com>
2022-10-22 21:24:02 +0300

    updating user document after note creation + sending cookie with userId and accessToken to client

--- pages/api/notes/index.js
+++ pages/api/notes/index.js
@@ -1,21 +1,21 @@
-import { createNote, handleCookieWithToken } from '../../../lib/api/services/notes.service';
+import { createNote } from '../../../lib/api/services/notes.service';
 import { apiMiddleware } from '../../../lib/api/middleware';
-import { extractCookie, validateAccessTokenAndGetUser } from '../../../lib/utils/auth';
+import {
+	extractCookie,
+	handleCookieWithToken,
+} from '../../../lib/utils/auth';
+import { handleUserAfterNoteCreation } from '../../../lib/api/services/user.service';
 
-const postHandler = async req => {
+const postHandler = async (req, res) => {
 	if (req.method === 'POST') {
 		const { content } = req.body;
 		const { token } = req.cookies;
+		const newNote = await createNote(content);
 		const userData = extractCookie(token);
-		const newNote = await createNote(userData, content);
-
-		// const finalCookie = handleCookieWithToken(
-		// 	req.cookies.notesTokens,
-		// 	newNoteData.id,
-		// 	newNoteData.noteToken,
-		// );
-		// res.setHeader('Set-Cookie', finalCookie);
-		// res.json({ id: newNoteData.id });
+		const user = await handleUserAfterNoteCreation(newNote._id, userData);
+		const { userId, accessToken } = user;
+		const finalCookie = handleCookieWithToken(userId, accessToken);
+		res.setHeader('Set-Cookie', finalCookie);
 		return newNote;
 	} else {
 		console.log('an error has occurred');
