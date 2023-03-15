794e5f3e56acf7210010a1fa97f9bd4193ccafc1
Daniel Alter <daniel.united@windowslive.com>
2023-03-15 21:10:09 +0200

    Visual improvements

--- src/lib/components/FlowButton.js
+++ src/lib/components/FlowButton.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 import brain from "./brain.PNG";
 
 export default class FlowButton extends React.Component {
-	render() {
-		return (
-			<Link to="/flow">
-				<button className="app-button">
-					<img src={brain} alt="מצב זרימה" />
-					<span className="app-button-tooltip">הכנס לפלואו</span>
-				</button>
-			</Link>
-		);
-	}
+  render() {
+    return (
+      <Link to="/flow">
+        <button className="app-button">
+          <img src={brain} alt="מצב זרימה" loading="eager" />
+          <span className="app-button-tooltip">הכנס לפלואו</span>
+        </button>
+      </Link>
+    );
+  }
 }
--- src/lib/components/SaveButton.js
+++ src/lib/components/SaveButton.js
@@ -2,12 +2,12 @@ import React from "react";
 import save from "./save.PNG";
 
 export default class SaveButton extends React.Component {
-	render() {
-		return (
-			<button onClick={this.props.onClick} className="app-button">
-				<img src={save} alt="שמירה" />
-				<span className="app-button-tooltip">שמירת פתק</span>
-			</button>
-		);
-	}
+  render() {
+    return (
+      <button onClick={this.props.onClick} className="app-button">
+        <img src={save} alt="שמירה" loading="eager" />
+        <span className="app-button-tooltip">שמירת פתק</span>
+      </button>
+    );
+  }
 }
